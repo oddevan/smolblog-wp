247e2222d5b4a463150ff9bdec787cd110f751e1
Evan Hildreth <evan.hildreth@webdevstudios.com>
2019-06-12 22:29:13 -0400

    Successfully authorized!

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-// Silence is golden, but these are the words that the world needs to hear.
+/**
+ * Silence is golden, but these are the words that the world needs to hear. - DC Talk
+ *
+ * @package Smolblog\WP
+ */
+
+http_response_code( 404 );
--- src/smolblog.php
+++ src/smolblog.php
@@ -9,8 +9,9 @@
 namespace Smolblog\WP;
 
 use WebDevStudios\OopsWP\Utility\Hookable;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
-require_once '../smolblog-config.php';
+require_once __DIR__ . '/../smolblog-config.php';
 
 /**
  * Main class for the Smolblog plugin
@@ -24,16 +25,27 @@ class Smolblog implements Hookable {
 	public function register_hooks() {
 			// Put your hooks here!
 			add_action( 'admin_menu', [ $this, 'add_smolblog_dashboard_page' ] );
-			add_action( 'admin_init', [ $this, 'handle_initiate_oauth' ] );
+			add_action( 'admin_init', [ $this, 'handle_admin_actions' ] );
 	}
 
-	public function handle_initiate_oauth() {
-		if ( isset( $_GET['smolblog_action'] ) ) {
-			switch( $_GET['smolboog_action'] ) {
-				case 'twitter_engage' :
-				$connection = new Abraham\TwitterOAuth\TwitterOAuth( SMOLBLOG_TWITTER_APPLICATION_KEY, SMOLBLOG_TWITTER_APPLICATION_SECRET );
-
-				
+	/**
+	 * Handle URLs of the format [WP]/wp-admin/admin.php/smolblog/...
+	 */
+	public function handle_admin_actions() {
+		if ( isset( $_SERVER['PATH_INFO'] ) ) {
+			$path_info = explode( '/', $_SERVER['PATH_INFO'] );
+			if ( 'smolblog' === $path_info[1] ) {
+				if ( 'oauth' === $path_info[2] ) {
+					if ( 'init' === $path_info[3] ) {
+						if ( 'twitter' === $path_info[4] ) {
+							$this->oauth_twitter_engage();
+						}
+					} elseif ( 'callback' === $path_info[3] ) {
+						if ( 'twitter' === $path_info[4] ) {
+							$this->oauth_twitter_callback();
+						}
+					}
+				}
 			}
 		}
 	}
@@ -59,10 +71,45 @@ class Smolblog implements Hookable {
 	public function smolblog_dashboard() {
 		echo '<h1>Smolblog</h1>';
 
-		if ( get_option( 'smolblog_twitter_user_key' ) ) {
-			echo '<p>Authenticated with Twitter as [account]</p>';
+		if ( get_option( 'smolblog_twitter_username' ) ) {
+			echo '<p>Authenticated with Twitter as ' . get_option( 'smolblog_twitter_username' ) . '</p>';
 		} else {
-			echo '<p><a href="?page=smolblog&amp;action=twitter-connect" class="button">Sign in with Twitter</a></p>';
+			echo '<p><a href="/wp-admin/admin.php/smolblog/oauth/init/twitter" class="button">Sign in with Twitter</a></p>';
 		}
+	}
+
+	public function oauth_twitter_engage() {
+		$callback_url = 'https://smolblog.local/wp-admin/admin.php/smolblog/oauth/callback/twitter';
+
+		$connection = new TwitterOAuth( SMOLBLOG_TWITTER_APPLICATION_KEY, SMOLBLOG_TWITTER_APPLICATION_SECRET );
+
+		$request_token = $connection->oauth( 'oauth/request_token', array( 'oauth_callback' => $callback_url ) );
+
+		set_transient( 'smolblog_twitter_oauth_request_' . get_current_blog_id(), $request_token, 5 * MINUTE_IN_SECONDS );
+
+		$url = $connection->url( 'oauth/authorize', array( 'oauth_token' => $request_token['oauth_token'] ) );
+
+		header( 'Location: ' . $url, true, 302 );
+		die;
+	}
+
+	public function oauth_twitter_callback() {
+		$request_token = get_transient( 'smolblog_twitter_oauth_request_' . get_current_blog_id() );
+
+		if ( isset( $_REQUEST['oauth_token'] ) && $request_token['oauth_token'] !== $_REQUEST['oauth_token'] ) {
+			wp_die( 'OAuth tokens did not match: Got "' . $_REQUEST['oauth_token'] . '" but expected "' . $request_token['oauth_token'] . '".' );
+		}
+
+		$connection = new TwitterOAuth( SMOLBLOG_TWITTER_APPLICATION_KEY,
+																		SMOLBLOG_TWITTER_APPLICATION_SECRET,
+																		$request_token['oauth_token'],
+																		$request_token['oauth_token_secret'] );
+
+		$access_token = $connection->oauth( 'oauth/access_token', [ 'oauth_verifier' => $_REQUEST['oauth_verifier'] ] );
+
+		update_option( 'smolblog_oauth_twitter', $access_token, false );
+		update_option( 'smolblog_twitter_username', $access_token['screen_name'], false );
+
+		header( 'Location: /wp-admin/admin.php?page=smolblog', true, 302 );
 	}
 }
